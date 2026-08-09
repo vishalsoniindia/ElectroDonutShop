2916c2d797bf2f242b49cb38e8ef92dc3da353a2
vishalsoniindia <vishalsoni8159@gmail.com>
2026-08-09 23:13:18 +0530

    rgb pendant images

--- data/products.js
+++ data/products.js
@@ -50,7 +50,7 @@ window.PRODUCTS = [
     tags: ["wearable", "kits", "coincell"],
     // badge: "Best seller",
     stock: "in_stock",
-    images: ["1.jpeg", "2.jpg" , "3.jpg" , "4.jpg"],
+    images: ["1.jpeg", "2.jpg" , "3.jpg" , "4.jpg" , "5.png" ],
     specs: {
       "Switches": "Switches for RGB",
       "Colors": "Select a Color from 7 Options",
